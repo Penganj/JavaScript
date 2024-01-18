d87d0669f49da483e2cd8016da76a8eba5e5dc46
Şevval Arslan <ismail.kurt7749@gmail.com>
2024-01-18 18:08:31 +0300

    12 advanced part 2

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -19,9 +19,12 @@ let score = (JSON.parse(localStorage.getItem('score'))) || {
   let isAutoPlaying = false;
   let intervalId;
 
+  //const autoPlay = () => {
+
+  //};
   function autoPlay() {
     if (!isAutoPlaying) {
-        intervalId = setInterval(function() {
+        intervalId = setInterval(() => {
         const playerMove = pickComputerMove();
         playGame(playerMove);
       }, 1000);
@@ -31,9 +34,22 @@ let score = (JSON.parse(localStorage.getItem('score'))) || {
       clearInterval(intervalId);
       isAutoPlaying = false;
     }
-
   }
 
+  document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('rock');
+  });
+
+  document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('paper');
+  });
+
+  document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    playGame('scissors');
+  });
 
   function playGame(playerMove) {
     const computerMove = pickComputerMove();
